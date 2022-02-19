2cbfead648168f4577c6ed98b9a72144ee7fde6b
1975km <km1975@yandex.ru>
2022-02-19 17:14:09 +0300

    Домашняя работа 3-1

---
+++ src/Arithmetic.java
@@ -0,0 +1,19 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+        public class Arithmetic {
+            int x;
+            int y;
+
+            void numX() {
+                if (x <= 0) {
+                    System.out.println("Вы должны ввести значение больше 0");
+                }
+            }
+            void numY() {
+                if (y <= 0) {
+                    System.out.println("Вы должны ввести значение больше 0");
+                }
+            }
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+            }
+    Arithmetic arithmetic = new Arithmetic();
+    arithmetic.x= scanner.nextInt();
+        arithmetic.numX();
+    arithmetic.y = scanner.nextInt();
+        arithmetic.numY();
+}
